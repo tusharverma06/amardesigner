74b842c0a1b13cf996dc22d79a0a3507fcf2c403
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-23 13:59:23 +0530

    overflow fixed

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,7 +4,7 @@ const Option = () => {
 
     return (
         <div className='w-full sm:w-11/12 max-w-[1058px] flex items-center justify-between my-12 mx-auto py-1 px-4 sm:px-2'>
-            <div className="flex items-center justify-between w-full gap-10 mx-auto overflow-x-auto overflow-y-hidden sm:gap-6 lg:overflow-x-visible" >
+            <div className="flex items-center justify-between w-full gap-10 mx-auto overflow-x-auto overflow-y-hidden sm:overflow-y-visible sm:gap-6 lg:overflow-x-visible" >
                 {options.map((option) => {
                     return <div className="flex flex-col items-center justify-center gap-1 transition-all cursor-pointer">
                         <img src={option.link} alt="" className='w-6 h-6' />
