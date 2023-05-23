d65c21a8ccd792995a0a9532a941ce09010bb3dc
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-23 13:22:50 +0530

    made home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { useState, useEffect } from 'react';
+import Option from './components/Options/Options';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/footer';
 function App() {
+   // Watching for windowWidth to make responsive
+   let width;
+   if (typeof window !== "undefined") {
+       width = window.innerWidth;
+   }
+   const [windowWidth, setWindowWidth] = useState(width);
+
+   useEffect(() => {
+       function watchWidth() {
+           setWindowWidth(window.innerWidth);
+       }
+
+       window.addEventListener("resize", watchWidth);
+   }, [windowWidth]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <Navbar windowWidth={windowWidth}/>
+      <Option />
+      <Projects />
+      <Footer />
+      </>
     </div>
   );
 }
