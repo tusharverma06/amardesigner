9619bd70f82599e8c8ed3746847c16167d4ee817
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-25 22:08:14 +0530

    Made the design-ideas main page

---
+++ src/components/DesignIdeas/dummyData.js
@@ -0,0 +1,47 @@
+export const DesignIdeas = [
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/living-d-1662025141-GvMP2.png',
+        title: 'Living Room',
+        noOfDesigns: 1892
+    },
+    {
+      thumbnail: "https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/kitchen-d-1662025139-loWXM.png",
+      title: 'Kitchen Design',
+      noOfDesigns: 1219 
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/mbr-d-1662025142-Upoli.png',
+        title: 'Hall Design',
+        noOfDesigns: 1287
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/puja-d-1662025144-2KtyX.png',
+        title: 'Pooja Room',
+        noOfDesigns: 2056
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/bathroom-2-d-1662025132-btVYp.png',
+        title: 'Bathroom Design',
+        noOfDesigns: 1734
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/balcony-d-1662025130-OsJOY.png',
+        title: 'Balcony Design',
+        noOfDesigns: 1325
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/destop-1673502929-FsYkr.jpg',
+        title: 'Tile Design',
+        noOfDesigns: 827
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/kbr-d-1662025138-0EGbI.png',
+        title: 'Kids Room Design',
+        noOfDesigns: 509
+    },
+    {
+        thumbnail: 'https://images.livspace-cdn.com/w:1920/plain/https://d3gq2merok8n5r.cloudfront.net/abhinav/design-ideas-thumbnails-1628773921-7vSz1/jas-thumbnails-1662014793-zEzY3/desktop-1662014816-DsTte/kbr-d-1662025138-0EGbI.png',
+        title: 'Kids Room Design',
+        noOfDesigns: 509
+    }
+]
