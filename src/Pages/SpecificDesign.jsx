8c2a5a22f20e4053210c760fb6569b0ba24039bd
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-05-29 11:33:13 +0530

    style-changed width of getQuote button and nav fix

--- src/Pages/SpecificDesign.jsx
+++ src/Pages/SpecificDesign.jsx
@@ -12,8 +12,8 @@ import { nameShortner } from '../utils/utils'
 const SpecificDesign = () => {
     const navigate = useNavigate()
     const location = useLocation()
-    const {thumbnail, DesignData, index} = location.state
-    // console.log(location.state)
+    const { thumbnail, DesignData, index, feet } = location.state
+    console.log(location.state)
     const { designCategory, designName } = useParams()
     const [isCopied, setIsCopied] = useState(false)
     const [isLiked, setIsLiked] = useState(false)
@@ -60,7 +60,7 @@ const SpecificDesign = () => {
         setIsOpen(false)
         setData(data => ({ ...data, name: '', email: '', pincode: '' }))
     }
-    
+
     // Navigating projects
     // const handlePreviousNavigation = () => {
     //     navigate(`/design-ideas/${designCategory}/${DesignData[index!==0 && index - 1]}`)
@@ -74,24 +74,26 @@ const SpecificDesign = () => {
             {/* Image thumbnail */}
             <div className='w-full xl:w-[60%] flex items-center flex-col justify-between gap-4'>
                 <img src={thumbnail} alt="" className="block h-full mx-auto xl:w-11/12 " />
-                <div className="flex items-center justify-between w-full px-12">
+                <div className="flex items-center justify-between w-full px-2">
                     <div className='flex items-center justify-between'>
-                        <button className='flex items-center justify-center gap-1'  onClick={null}>
+                        <button className='flex items-center justify-center gap-1' onClick={null}>
                             <img src={BackArrow} alt="" className='w-8 h-8 -rotate-90' />
-                            <img src={thumbnail} alt="" className='w-20 h-10' />
+                            <img src={thumbnail} alt="" className='h-8 w-14 sm:w-20 sm:h-10' />
                         </button>
                         <div className="flex flex-col items-start justify-start gap-1">
-                            <span className="px-2 text-base font-medium">Previous </span>
-                            <span className="px-2 text-base font-medium"> Kitchen design</span>
-                        </div>
+                            <span className="px-2 text-sm font-medium sm:text-base">Previous </span>
+                            <span className="block px-2 text-xs font-medium w-max sm:hidden">{nameShortner('Classic L-Shaped Modular Kitchen Design With Patterned Moroccan Tiles', 10)}</span>
+                            <span className="hidden px-2 text-base font-medium sm:block">{nameShortner('Classic L-Shaped Modular Kitchen Design With Patterned Moroccan Tiles', 25)}</span>
+                            </div>
                     </div>
                     <div className='flex items-center justify-between'>
                         <div className="flex flex-col items-end justify-start gap-1">
-                            <span className="px-2 text-base font-medium">Next</span>
-                            <span className="px-2 text-base font-medium"> Bathroom design</span>
+                            <span className="px-2 text-sm font-medium sm:text-base">Next</span>
+                            <span className="block px-2 text-xs font-medium w-max sm:hidden">{nameShortner('Modern Modular L-Shaped Kitchen Design With Open Storage', 10)}</span>
+                            <span className="hidden px-2 text-base font-medium sm:block">{nameShortner('Modern Modular L-Shaped Kitchen Design With Open Storage', 25)}</span>
                         </div>
-                        <button className='flex items-center justify-center gap-1'  onClick={null}>
-                            <img src={'https://images.livspace-cdn.com/w:1440/dpr:1/q:100/plain/https://d3ai42rl8fy79o.cloudfront.net/media/public/da196bb2-e162-4707-bf2e-4acadb78de65.jpg'} alt="" className='w-20 h-10' />
+                        <button className='flex items-center justify-center gap-1' onClick={null}>
+                            <img src={'https://images.livspace-cdn.com/w:1440/dpr:1/q:100/plain/https://d3ai42rl8fy79o.cloudfront.net/media/public/da196bb2-e162-4707-bf2e-4acadb78de65.jpg'} alt="" className='h-8 w-14 sm:w-20 sm:h-10' />
                             <img src={BackArrow} alt="" className='w-8 h-8 rotate-90' />
                         </button>
                     </div>
@@ -99,15 +101,15 @@ const SpecificDesign = () => {
             </div>
             {/* Design  Details  */}
             <div className="flex flex-col items-start justify-center w-full  xl:w-[40%] ">
-                    {/* Navigation */}
-                    <div className="flex items-start justify-start w-full px-4 mx-auto mt-4 xl:px-0">
-                        <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={'/'}>Home/</Link>
-                        <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={'/design-ideas'}>Design-Ideas/</Link>
-                        <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={`/design-ideas/${designCategory}`}>{designCategory}/</Link>
-                        <Link className="font-medium text-gray-500 text-[8px] sm:text-sm md:text-base hover:text-gray-500" to={`/design-ideas/${designCategory}/${designName}`}>{nameShortner(designName, 20)}</Link>
-                    </div>
+                {/* Navigation */}
+                <div className="flex items-start justify-start w-full px-4 pt-10 xl:pt-24 ">
+                    <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={'/'}>Home/</Link>
+                    <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={'/design-ideas'}>Design-Ideas/</Link>
+                    <Link className="font-medium text-red-500 text-[8px] sm:text-sm md:text-base hover:text-red-500" to={`/design-ideas/${designCategory}`}>{designCategory}/</Link>
+                    <Link className="font-medium text-gray-500 text-[8px] sm:text-sm md:text-base hover:text-gray-500" to={`/design-ideas/${designCategory}/${designName}`}>{nameShortner(designName, 20)}</Link>
+                </div>
                 <div className='flex items-center justify-between w-full h-full gap-6 px-4 overflow-y-auto xl:pr-12'>
-                    <h1 className='pt-8 text-xl font-black text-start md:max-w-2xl sm:text-2xl xl:text-3xl xl:max-w-lg'>{designName}</h1>
+                    <h1 className='pt-8 text-xl font-black text-start md:max-w-3xl sm:text-2xl xl:text-3xl xl:max-w-lg'>{designName}</h1>
                     <div className="flex items-center justify-center gap-2">
                         <button className={`relative flex items-center justify-center w-8 h-8 lg:w-10 lg:h-10 border border-gray-500 rounded-full`} onClick={handleShare}>
                             <img src="https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSbB_7FEkfvGkYYnk8bYlE8046nTjiDWcUrt85XE6r476kCVgxuJQmYs2RNnCYTEnG-" alt="" className="block w-4 h-4 mx-auto lg:w-6 lg:h-6" />
@@ -161,9 +163,10 @@ const SpecificDesign = () => {
                                 Pullout drawers and tall storage units                            </span>
                         </li>
                     </ul>
+                    <span className="text-lg font-medium">Size: {feet} feet</span>
                 </div>
                 <div className='flex items-center justify-center w-full my-12'>
-                    <button className='px-20 py-5 mx-auto font-bold text-center text-white bg-red-500 rounded-xl' onClick={handleClick}>Get Free Quote</button>
+                    <button className='w-11/12 px-20 py-4 mx-auto font-bold text-center text-white bg-red-500 rounded-2xl' onClick={handleClick}>Get Free Quote</button>
                 </div>
             </div>
             <Modal handleCancel={handleCancel} data={data} inputHandler={onInput} isOpen={isOpen} setIsOpen={setIsOpen} handleClick={handleData} />
